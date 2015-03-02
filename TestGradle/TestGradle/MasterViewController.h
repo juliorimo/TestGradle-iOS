c0b322f77a1bf755460ed0a840d7d1cbae26be37
Julio Rivas <julio@lualabs.com>
2015-03-02 12:49:01 +0100

    Add proyect

---
+++ TestGradle/TestGradle/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  TestGradle
+//
+//  Created by Julio Rivas on 2/3/15.
+//  Copyright (c) 2015 Julio Rivas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
